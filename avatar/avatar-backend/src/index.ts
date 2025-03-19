d188b3025c95d53786093ff9713b1169e70fc535
roeepaiss <roeepaiss@gmail.com>
2025-03-19 12:36:06 +0200

    change logs and add cors

--- avatar/avatar-backend/src/index.ts
+++ avatar/avatar-backend/src/index.ts
@@ -1,5 +1,8 @@
 import app from "./server.js";
 
-app.listen(8080, () => {
-    console.log("hello world")
+const port = Number(process.env.PORT || 8080);
+
+app.listen(port, () => {
+    console.log(`[Server]: Server is running at http://localhost:${port}`)
+    console.log(`[Server]: Server is running at http://0.0.0.0:${port}`)
 });
--- avatar/avatar-backend/src/server.ts
+++ avatar/avatar-backend/src/server.ts
@@ -1,10 +1,14 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-
+app.use(
+  cors({
+    origin: [process.env.CLIENT_URL || ""],
+  })
+);
 
 export default app;

cae4361cb9fb0549d638be76abfe8dfa352fa216
roeepaiss <roeepaiss@gmail.com>
2025-03-19 18:57:39 +0200

    add basic services selection

--- avatar/avatar-backend/src/repositories/svc.repository.ts
+++ avatar/avatar-backend/src/repositories/svc.repository.ts
@@ -1,9 +1,12 @@
+import { services } from "../db/schema.js";
 import db from "../db/db.js";
 
-export const getAllServices = () => {
-    // try {
-
-    // }
+export const getServices = async () => {
+  try {
+    return await db.select().from(services).execute();
+  } catch (error) {
+    throw error;
+  }
 };
 
-export default { getAllServices };
+export default { getServices };
--- avatar/avatar-backend/src/routers/svc.router.ts
+++ avatar/avatar-backend/src/routers/svc.router.ts
@@ -1,5 +1,15 @@
 import express from "express";
+import serviceService from "../services/svc.service.js";
 
 const router = express.Router();
+
+router.get("/services", async (req, res, next) => {
+  try {
+    const results = await serviceService.getServices();
+    res.json(results);
+  } catch (error) {
+    console.log(error)
+  }
+});
 
 export default router;

3c3f865fa6b640a8390567ceabd2fd9aa442bc0b
roeepaiss <roeepaiss@gmail.com>
2025-03-20 09:16:50 +0200

    add api health check

--- avatar/avatar-backend/src/routers/index.ts
+++ avatar/avatar-backend/src/routers/index.ts
@@ -1,10 +1,17 @@
 import express from "express";
 import orderRouter from "./order.router.js";
 import svcRouter from "./svc.router.js";
+import logger from "../logger.js";
+import { StatusCodes } from "http-status-codes";
 
 const router = express.Router();
 
 router.use(orderRouter);
 router.use(svcRouter);
+
+router.use("/", (req, res) => {
+  logger.debug("hello world!");
+  res.status(StatusCodes.OK).send("hello world!");
+});
 
 export default router;

f0bd45e9378b40dbb02322459ad5933334e98b68
roeepaiss <roeepaiss@gmail.com>
2025-03-24 11:26:12 +0200

    finish first integration

--- avatar-frontend/src/api/rundeck/rundeckApi.ts
+++ avatar-frontend/src/api/rundeck/rundeckApi.ts
@@ -1,18 +1,25 @@
 import api from "../axios";
-import { Job, Execution, JobRunOptions } from "./types";
+import {
+  Job,
+  Execution,
+  JobRunOptions,
+  ProjectJob,
+  ExecutionLogEntry,
+} from "./types";
 
 const RUNDECK_BASE_PATH = "/rundeck";
 
 export const rundeckApi = {
-  getJobs: async (): Promise<Job[]> => {
-    const response = await api.get<Job[]>(`${RUNDECK_BASE_PATH}/jobs`);
+  getJobs: async (): Promise<ProjectJob[]> => {
+    const response = await api.get<ProjectJob[]>(`${RUNDECK_BASE_PATH}/jobs`);
+    return response.data;
+  },
+  getJobById: async (jobId: string): Promise<Job> => {
+    const response = await api.get<Job>(`${RUNDECK_BASE_PATH}/jobs/${jobId}`);
     return response.data;
   },
 
-  runJob: async (
-    jobId: string,
-    options: JobRunOptions
-  ): Promise<string> => {
+  runJob: async (jobId: string, options: JobRunOptions): Promise<string> => {
     const response = await api.post<string>(
       `${RUNDECK_BASE_PATH}/jobs/${jobId}/run`,
       { options }
@@ -23,6 +30,14 @@ export const rundeckApi = {
   getExecutions: async (): Promise<Execution[]> => {
     const response = await api.get<Execution[]>(
       `${RUNDECK_BASE_PATH}/executions`
+    );
+    return response.data;
+  },
+  getExecutionLogs: async (
+    executionId: string
+  ): Promise<ExecutionLogEntry[]> => {
+    const response = await api.get<ExecutionLogEntry[]>(
+      `${RUNDECK_BASE_PATH}/executions/${executionId}/output`
     );
     return response.data;
   },
